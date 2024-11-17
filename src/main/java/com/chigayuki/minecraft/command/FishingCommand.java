f1aba2885be871dd116e0b30e0feeba854af7315
chigayuki <yukinari_22n1101244@nnn.ed.jp>
2024-11-18 02:34:40 +0900

    速度アップ

--- src/main/java/com/chigayuki/minecraft/command/FishingCommand.java
+++ src/main/java/com/chigayuki/minecraft/command/FishingCommand.java
@@ -6,10 +6,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FishingCommand implements CommandExecutor {
   private final JavaPlugin plugin;
+  private Player player;
 
   public FishingCommand(JavaPlugin plugin) {
     this.plugin = plugin;
@@ -25,15 +27,16 @@ public class FishingCommand implements CommandExecutor {
     FishingListener fishingListener = new FishingListener();
     plugin.getServer().getPluginManager().registerEvents(fishingListener, plugin);
     new CountDownBar(
-              "釣りミニゲーム",
-              player,
-              60,
-              plugin,
-              () -> {
-                plugin.getServer().getPluginManager().disablePlugin(plugin);
-                player.sendMessage("釣りミニゲーム終了！");
-              }
-            );
+            "釣りミニゲーム",
+            player,
+//            600,
+            60, // デバック用
+            plugin,
+            () -> {
+              HandlerList.unregisterAll(fishingListener);
+              player.sendMessage("釣りミニゲーム終了！");
+            }
+    );
     return false;
   }
 }
--- src/main/java/com/chigayuki/minecraft/listeners/FishingListener.java
+++ src/main/java/com/chigayuki/minecraft/listeners/FishingListener.java
@@ -1,7 +1,9 @@
 package com.chigayuki.minecraft.listeners;
 
 import com.chigayuki.minecraft.lib.FishingItems;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.FishHook;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -15,18 +17,28 @@ public class FishingListener implements Listener {
   public void OnPlayerFishEvent(PlayerFishEvent event) {
     Player player = event.getPlayer();
     player.sendMessage("釣りの状態: " + event.getState());
-    if (event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
-      player.sendMessage("魚を釣り上げました。");
-      ItemStack itemStack = FishingItems.getRandomFish().getItem();
-      Item droppedItem = player.getWorld().dropItem(event.getHook().getLocation(), itemStack);
-      droppedItem.setPickupDelay(0);
-      applyVelocity(player, droppedItem);
+    switch (event.getState()) {
+      case FISHING:
+        FishHook hook = event.getHook();
+        int maxWaitTime = hook.getMaxWaitTime();
+        int LureBonus = getLureBonus(player, hook);
+        hook.setMaxWaitTime(100 + Math.max(maxWaitTime / 3 - LureBonus, 0));
+        hook.setMinWaitTime(10);
+        break;
+      case CAUGHT_FISH:
+        ItemStack itemStack = FishingItems.getRandomFish().getItem();
+        Item droppedItem = player.getWorld().dropItem(event.getHook().getLocation(), itemStack);
+        droppedItem.setPickupDelay(0);
+        applyVelocity(player, droppedItem);
 
-      event.getHook().setHookedEntity(droppedItem);
+        event.getHook().setHookedEntity(droppedItem);
 
-      if (event.getCaught() != null) {
-        event.getCaught().remove();
-      }
+        if (event.getCaught() != null) {
+          event.getCaught().remove();
+        }
+        break;
+      default:
+        break;
     }
   }
 
@@ -39,5 +51,18 @@ public class FishingListener implements Listener {
     double speedMultiplier = Math.min(distance * 0.11, 2.5);
     direction.setY(yBoost);
     entity.setVelocity(direction.normalize().multiply(speedMultiplier));
+  }
+
+  private int getLureBonus(Player player, FishHook fishHook) {
+    ItemStack fishingRod = player.getInventory().getItemInMainHand();
+    if (!fishingRod.getType().getKey().getKey().equalsIgnoreCase("fishing_rod")) {
+      return 0;
+    }
+    int lureLevel = fishingRod.getEnchantmentLevel(Enchantment.LURE);
+    if (lureLevel > 0) {
+      fishHook.setApplyLure(false);
+      return lureLevel * 100;
+    }
+    return 0;
   }
 }
